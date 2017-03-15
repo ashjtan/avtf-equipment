08d30a616caaa2255c9b35b0057a05824086307c
Ashley Tan <tan.ashley32@gmail.com>
2017-03-15 17:17:43 -0400

    pt. 2

--- src/tan/five/model/CSVWriter.java
+++ src/tan/five/model/CSVWriter.java
@@ -1,5 +1,27 @@
 package tan.five.model;
 
-public class CSVWriter {
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
+
+public class CSVWriter {
+    public static void main(String[]args) throws FileNotFoundException{
+        PrintWriter pw = new PrintWriter("Sample Equipment.csv");
+        StringBuilder sb = new StringBuilder();
+        sb.append("id");
+        sb.append(',');
+        sb.append("Name");
+        sb.append('\n');
+
+        sb.append("1");
+        sb.append(',');
+        sb.append("Prashant Ghimire");
+        sb.append('\n');
+
+        pw.write(sb.toString());
+        pw.close();
+        System.out.println("done!");
+    }
 }

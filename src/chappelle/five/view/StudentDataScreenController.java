64ffdf785ca64c487c1729002c14a14e42680189
ChapmanChappelle <chapmanchappelle@chapmans-mbp.fcs.org>
2017-03-16 14:24:09 -0400

    beginning the tableview

---
+++ src/chappelle/five/view/StudentDataScreenController.java
@@ -0,0 +1,117 @@
+/*Chappelle, Chapman
+ * Period 5
+ * PSET 4-0
+ * December 7, 2016
+ */
+
+package chappelle.five.view;
+
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import java.io.IOException;
+import chapman.five.MainApp;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TableColumn;
+import javafx.beans.value.ChangeListener;
+
+
+
+public class StudentDataScreenController {  
+
+	//Controls
+	@FXML
+	public TableColumn colName;
+
+	//So the main can reference itself to the controllers
+	private MainApp mainApp;
+
+	public StudentDataScreenController() {
+		//loader needs this
+	}
+
+	//to initialize anything on start
+	@FXML 
+	private void initialize(){
+
+	}
+
+	
+	//TableView
+	colName.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<String[], String>, ObservableValue<String>>() {
+	    @Override
+	    public ObservableValue<String> call(TableColumn.CellDataFeatures<String[], String> p) {
+	        String[] x = p.getValue();
+	        if (x != null && x.length>0) {
+	            return new SimpleStringProperty(x[0]);
+	        } else {
+	            return new SimpleStringProperty("<no name>");
+	        }
+	    }
+
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	// @param mainApp
+	public void setMainApp(MainApp mainApp) {
+
+		//This sets the field for this controller to the application passed in:  
+		//This allows the main application to have a pointer to itself by setting its own pointer into the controller's field
+		this.mainApp = mainApp;
+	}
+}

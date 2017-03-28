63f24f1786260f6741b2771d8c8c2b5a96847adc
ChapmanChappelle <chapmanchappelle@Chapmans-MacBook-Pro.local>
2017-03-28 15:48:41 -0400

    added a back button to itemcheckincontroller

--- src/chappelle/five/view/ItemCheckInController.java
+++ src/chappelle/five/view/ItemCheckInController.java
@@ -1,5 +1,20 @@
 package chappelle.five.view;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ItemCheckInController {
+
+	@FXML
+	private Button btnBackButton;
+
+	//Method for switching scenes
+	@FXML
+	public void backButton() throws IOException {
+		SceneSwitcher.handleSceneSwitch(btnBackButton, "/chappelle/five/view/StudentWelcomeScreen.fxml");
+	}
+
 
 }

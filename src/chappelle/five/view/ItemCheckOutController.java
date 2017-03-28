7c7437b3d9138560870583041bb053006e37e90c
ChapmanChappelle <chapmanchappelle@754stu-0265834.fcs.org>
2017-03-28 13:58:19 -0400

    hi

--- src/chappelle/five/view/ItemCheckOutController.java
+++ src/chappelle/five/view/ItemCheckOutController.java
@@ -22,16 +22,13 @@ import javafx.stage.Stage;
 import tan.five.mainApp;
 import tan.five.model.Student;
 import tan.five.model.StudentEquipmentManagement;
+import chappelle.five.view.SceneSwitcher;
 
 public class ItemCheckOutController  {
 
 
 	@FXML
-	private Button btnCheckIn;
-	@FXML
-	private Button btnCheckOut;
-	@FXML
-	private Button btnGoBack;
+	private Button btnBackButton;
 
 	static ObservableList<Student> mySl;
 
@@ -39,35 +36,10 @@ public class ItemCheckOutController  {
 
 	//Method for switching scenes
 	@FXML
-	private void handleSceneSwitch(ActionEvent event) throws IOException{
-		Stage stage; 
-		Parent root;
-		//Setting the scene to the check in menu
-		if(event.getSource()==btnCheckIn){
-			//get reference to the button's stage         
-			stage=(Stage) btnCheckIn.getScene().getWindow();
-			//load up OTHER FXML document
-			root = FXMLLoader.load(getClass().getResource("CHECK IN MENU"));
-		}
-		//Setting the scene to the check out menu
-		else {
-			if(event.getSource()==btnCheckOut){
-				//get reference to the button's stage         
-				stage=(Stage) btnCheckOut.getScene().getWindow();
-				//load up OTHER FXML document
-				root = FXMLLoader.load(getClass().getResource("CHECK OUT MENU"));
-			}
-			//So it may set the root scene again
-			else{
-				stage=(Stage) btnGoBack.getScene().getWindow();
-				root = FXMLLoader.load(getClass().getResource("tan/five/view/Login.fxml"));
-			}
-			//create a new scene with root and set the stage
-			Scene checkInMenu = new Scene(root);
-			stage.setScene(checkInMenu);
-			stage.show();
-		}
+	public void backButton() throws IOException {
+		SceneSwitcher.handleSceneSwitch(btnBackButton, "/chappelle/five/view/StudentWelcomeScreen.fxml");
 	}
+	
 
 
 

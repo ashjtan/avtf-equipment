d90da92e15a9366e6e88c2d5798aadbd22dea5f3
Ashley Tan <tan.ashley32@gmail.com>
2017-03-29 16:49:18 -0400

    great

--- src/chappelle/five/view/ItemCheckOutController.java
+++ src/chappelle/five/view/ItemCheckOutController.java
@@ -2,16 +2,11 @@ package chappelle.five.view;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -20,11 +15,6 @@ import javafx.scene.control.SplitPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import tan.five.mainApp;
 import tan.five.model.Equipment;
@@ -122,14 +112,6 @@ public class ItemCheckOutController  {
 	}
 
 
-<<<<<<< HEAD
-=======
-
-
-
-
-
->>>>>>> refs/remotes/origin/master
 
 	//Gets ArrayList<Equipment> of items in cart checking out
 	public static ArrayList<Equipment> getEquipmentToCheckOut() {
